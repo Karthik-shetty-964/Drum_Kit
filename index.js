eef34256ae07b1cd843becc6dae0c4f3c9903a08
Karthik shetty <72199770+Karthik-shetty-964@users.noreply.github.com>
2023-09-22 21:00:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+
+function choice(drumChoice) {
+    switch (drumChoice) {
+        case "w":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+var btns = document.querySelectorAll('.drum');
+
+for(var btn of btns) {
+    btn.addEventListener("click", function () {
+        
+        var drumChoice = this.textContent;
+        choice(drumChoice);
+        addAnimation(drumChoice);
+    });
+}
+
+document.addEventListener('keydown', function (event) {
+    choice(event.key);
+    addAnimation(event.key);
+});
+
+function addAnimation(key) {
+    var activeButton = document.querySelector("."+key);
+    activeButton.classList.add('pressed');
+
+    setTimeout(() =>{
+        activeButton.classList.remove('pressed');
+    }, 100);
+
+}
